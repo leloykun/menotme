6db8681445b06ad6b64ef5225b3be74cdf63c901
Franz Louis Cesista <franzlouiscesista@gmail.com>
2019-03-23 19:49:36 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, jsonify, render_template
+
+import _pickle as cPickle
+import tensorflow.keras as tfk
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'DontTellAnyone'
+
+vectorizer = None
+model = None
+
+def load_model():
+  global vectorizer, model
+  with open('vectorizer.pkl', 'rb') as f:
+    vectorizer = cPickle.load(f)
+  model = tfk.models.load_model('menotme_neuralnetwork.h5')
+
+def predict(sentence):
+    print(sentence)
+    sentence_t = vectorizer.transform([sentence])
+    pred = model.predict(sentence_t)
+    print(pred)
+    return pred[0][0]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/query', methods=['GET', 'POST'])
+def query():
+    sentence = request.args.get("sentence", None)
+    if sentence is None:
+        sentence = request.json["sentence"]
+    pred = predict(sentence)
+    return jsonify(sentence=sentence,
+                   decision="true" if pred >= 0.5 else "false")
+
+if __name__ == '__main__':
+  load_model()
+  print(predict("HAHAHA"))
+  app.run(debug=False)
